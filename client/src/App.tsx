b02da445855e32bbcbe11f8a41b7fc5663bd468b
keniareis <keniapubg@gmail.com>
2024-09-29 02:54:04 -0300

    feat: deck screen with options button new, edit and add card on the decks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Flashcards from './pages/Flashcards';
-import Profile from './components/Profile';
 import Settings from './pages/Settings';
 import Home from './pages/Home';
+import Decks from './pages/Decks';
 
 const App: React.FC = () => {
   return (
@@ -15,7 +15,7 @@ const App: React.FC = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/flashcards" element={<Flashcards />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/decks" element={<Decks/>} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
