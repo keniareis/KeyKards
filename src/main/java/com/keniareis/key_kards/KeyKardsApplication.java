adb85af49cacf2b9435958b11d4aad3db5e7a2e1
Kênia <128853726+keniareis@users.noreply.github.com>
2025-05-26 20:52:43 -0300

    feat: backend begin

---
+++ src/main/java/com/keniareis/key_kards/KeyKardsApplication.java
@@ -0,0 +1,13 @@
+package com.keniareis.key_kards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyKardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyKardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keniareis/key_kards/KeyKardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keniareis.key_kards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyKardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
